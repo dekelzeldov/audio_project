97ce13b6e32093bc93e1570d1dc9e07a43112dc0
dekelzeldov <dekelzeldov@gmail.com>
2024-12-10 22:52:59 +0100

    latest

--- mood_clasification_train.py
+++ mood_clasification_train.py
@@ -5,15 +5,43 @@ from transformers import AutoModel, Wav2Vec2FeatureExtractor, TrainingArguments,
 from transformers.modeling_outputs import TokenClassifierOutput
 import numpy as np
 import evaluate
-from datasets import load_dataset
+from datasets import load_dataset, DatasetDict
 import torch
+from sklearn.model_selection import KFold
 
-# load dataset
-dataset_id = "Rehead/DEAM_stripped_vocals"
-dataset = load_dataset(dataset_id, split='train+test') # add [:20] to loads 20 examples
-dataset = dataset.train_test_split(test_size=0.1, seed=24)  # Splits loaded examples into two splits: if test_size=0.2 and 20 examples: train = 16 examples, test = 4 examples
-# ok so the predictions depends on the size of the test set
-# currently set so test receives the exact amount or expected predictions.
+# >>> X = np.array([[1, 2], [3, 4], [1, 2], [3, 4]])
+# >>> y = np.array([1, 2, 3, 4])
+# >>> kf = KFold(n_splits=2)
+# >>> kf.get_n_splits(X)
+# 2
+# >>> print(kf)
+# KFold(n_splits=2, random_state=None, shuffle=False)
+# >>> for i, (train_index, test_index) in enumerate(kf.split(X)):
+# ...     print(f"Fold {i}:")
+# ...     print(f"  Train: index={train_index}")
+# ...     print(f"  Test:  index={test_index}")
+# Fold 0:
+#   Train: index=[2 3]
+#   Test:  index=[0 1]
+# Fold 1:
+#   Train: index=[0 1]
+#   Test:  index=[2 3]
+
+DATA = False
+
+if DATA:
+    # load dataset
+    dataset_id = "Rehead/DEAM_stripped_vocals"
+    dataset = load_dataset(dataset_id, split='train+test') # add [:20] to loads 20 examples
+    dataset = dataset.train_test_split(test_size=0.1, seed=24)  # Splits loaded examples into two splits: if test_size=0.2 and 20 examples: train = 16 examples, test = 4 examples
+
+
+    from datasets import load_dataset
+
+    # First make the kfold object
+    kf = KFold(n_splits=10, shuffle=True)
+    num_k_fold_ephocs = 3
+
 
 
 ##########################################################################################################################
@@ -24,14 +52,15 @@ model_id = "m-a-p/MERT-v1-95M"
 # Loading the corresponding preprocessor config
 processor = Wav2Vec2FeatureExtractor.from_pretrained(model_id, trust_remote_code=True)
 
-# Make sure the sample_rate aligned
-sampling_rate = dataset["train"][0]["audio"]["sampling_rate"]
-resample_rate = processor.sampling_rate
-if resample_rate != sampling_rate:
-    dataset = dataset.cast_column("audio", Audio(sampling_rate=resample_rate))
+if DATA:
+    # Make sure the sample_rate aligned
+    sampling_rate = dataset["train"][0]["audio"]["sampling_rate"]
+    resample_rate = processor.sampling_rate
+    if resample_rate != sampling_rate:
+        dataset = dataset.cast_column("audio", Audio(sampling_rate=resample_rate))
 
 # Extract features
-max_duration = 10.0
+max_duration = 45
 def preprocess_function(examples):
     device = "cuda:0"
     audio_arrays = [x["array"] for x in examples["audio"]]
@@ -46,14 +75,15 @@ def preprocess_function(examples):
     )
     return inputs.to(device)
 
-dataset_encoded = dataset.map(
-    preprocess_function,
-#    remove_columns=['audio', 'valence_std', 'valence_max_mean', 'valence_max_std', 'valence_min_mean', 'valence_min_std',   # These can be commented out since unused columns are removed
-#                             'arousal_std', 'arousal_max_mean', 'arousal_max_std', 'arousal_min_mean', 'arousal_min_std'],
-    batched=True,
-    batch_size=100,
-    num_proc=1
-)
+if DATA:
+    dataset_encoded = dataset.map(
+        preprocess_function,
+    #    remove_columns=['audio', 'valence_std', 'valence_max_mean', 'valence_max_std', 'valence_min_mean', 'valence_min_std',   # These can be commented out since unused columns are removed
+    #                             'arousal_std', 'arousal_max_mean', 'arousal_max_std', 'arousal_min_mean', 'arousal_min_std'],
+        batched=True,
+        batch_size=100,
+        num_proc=1
+    )
 
 ##########################################################################################################################
 # 
@@ -80,7 +110,7 @@ class CustomModel(torch.nn.Module):
             #device_map="auto",
             output_scores = True
         )
-    self.dropout = torch.nn.Dropout(0.1) 
+    self.dropout = torch.nn.Dropout(0.5) 
     self.classifier = torch.nn.Linear(768, len(label2id)) # load and initialize weights
 
   def forward(self, input_values=None, attention_mask=None,labels=None):
@@ -105,11 +135,14 @@ device = "cuda:0"
 model = model.to(device)
 
 model_name = model_id.split("/")[-1]
-dataset_name = dataset_id.split("/")[-1]
-path = f"{model_name}-finetuned-{dataset_name}"
+if DATA:
+    dataset_name = dataset_id.split("/")[-1]
+    path = f"{model_name}-finetuned-{dataset_name}"
+else:
+    path = f"{model_name}-finetuned"
 batch_size = 1
 gradient_accumulation_steps = 8
-num_train_epochs = 10
+num_train_epochs = 1
 
 training_args = TrainingArguments(
     path,
@@ -162,89 +195,110 @@ class CustomTrainer(Trainer):
 
         return (loss, outputs) if return_outputs else loss
 
-trainer = CustomTrainer(
-    model,
-    training_args,
-    train_dataset=dataset_encoded["train"],
-    eval_dataset=dataset_encoded["test"],
-    processing_class=processor,
-    compute_metrics=compute_metrics,
-)
-
-LOAD = False
-TRAIN = True
-SAVE = True
+if DATA:
+    trainer = CustomTrainer(
+        model,
+        training_args,
+        train_dataset=dataset_encoded["train"],
+        eval_dataset=dataset_encoded["test"],
+        processing_class=processor,
+        compute_metrics=compute_metrics,
+    )
 
 
-file_name = f'model_weights_latest.pth'
+LOAD = True
+TRAIN = False
+SAVE = False
+
+
+file_name = f'model_weights_test.pth'
 
 if(LOAD):
-    
-    print()
-    print("Pre-loading evaluation:")
-    model.eval()
-    print(trainer.evaluate())
-
     print()
     print(f"loading from {file_name}")
     model.load_state_dict(torch.load(file_name, weights_only=True))
 
 if(TRAIN):
+    print()
     
-    print()
-    print("Pre-training evaluation:")
-    model.eval()
-    print(trainer.evaluate())
-
-    print()
-    model.train()
     print("training")
-    trainer.train()
+    model.train()
+    splits = kf.split(dataset_encoded["train"])
+    for i, (train_idxs, val_idxs) in enumerate(splits): 
+        print(f"Fold #: {i}")
+        fold_dataset = DatasetDict({
+            "train": dataset_encoded["train"].select(train_idxs),
+            "validation": dataset_encoded["train"].select(val_idxs),
+        })
+        trainer.train_dataset = fold_dataset["train"]
+        trainer.eval_dataset = fold_dataset["validation"]
+        trainer.train()
+
 
 if(SAVE):
     print()
     print(f"saving to {file_name}")
     torch.save(model.state_dict(), file_name)
   
+if DATA:
+    trainer.eval_dataset = dataset_encoded["test"]
+    print("testing")
+    model.eval()
+    print(trainer.evaluate())
 
+print()
+print()
+print("**********************")
+print("evaluating custom data")
+print("**********************")
 
 from scipy.io import wavfile
 import torchaudio.transforms as T
+import os
 
-input_sr, input_array = wavfile.read('acoustic-guitar-loop-f-91bpm-132687.wav')
+# assign directory
+directory = '.\AudioDataset\AudioDataset'
 
-resample_rate = processor.sampling_rate
-# make sure the sample_rate aligned
-if resample_rate != input_sr:
-    print(f'setting rate from {input_sr} to {resample_rate}')
-    resampler = T.Resample(input_sr, resample_rate)
-else:
-    resampler = None
+# iterate over files in
+# that directory
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        print(f)
 
-# audio file is decoded on the fly
-if resampler:
-    input_array = resampler(torch.from_numpy(input_array.astype(dtype='f4')))
+        input_sr, input_array = wavfile.read('acoustic-guitar-loop-f-91bpm-132687.wav')
 
-print(input_array.size())
-if input_array.size()[-1] == 2:
-    # convert sterio to mono by summing
-    input_array = input_array.sum(axis=-1)
-print(input_array.size())
+        resample_rate = processor.sampling_rate
+        # make sure the sample_rate aligned
+        if resample_rate != input_sr:
+            print(f'setting rate from {input_sr} to {resample_rate}')
+            resampler = T.Resample(input_sr, resample_rate)
+        else:
+            resampler = None
 
-device = "cuda:0"
-inputs = processor(
-    input_array,
-    sampling_rate=processor.sampling_rate,
-    max_length=int(processor.sampling_rate * max_duration),
-    truncation=True,
-    return_attention_mask=True,
-    return_tensors="pt",
-    padding=True
-)
-inputs.to(device)
+        # audio file is decoded on the fly
+        if resampler:
+            input_array = resampler(torch.from_numpy(input_array.astype(dtype='f4')))
 
-with torch.no_grad():
-    outputs = model(**inputs)
+        if input_array.size()[-1] == 2:
+            # convert sterio to mono by summing
+            input_array = input_array.sum(axis=-1)
 
-print(outputs)
+        device = "cuda:0"
+        inputs = processor(
+            input_array,
+            sampling_rate=processor.sampling_rate,
+            max_length=int(processor.sampling_rate * max_duration),
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors="pt",
+            padding=True
+        )
+        inputs.to(device)
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        print(f"\tvalance: {outputs.logits[0][0]}, arousal:{outputs.logits[0][1]}")
 
